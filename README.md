abbca4f6747198cdff74fba149d1809cb0dbe22d
Thomas Stringer <git@trstringer.com>
2018-04-16 15:21:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Kubernetes Custom Controller - Core Resource Handling
+
+## What is this?
+
+An example of a custom Kubernetes controller that's only purpose is to watch for the creation, updating, or deletion of all pods (in the default namespace). This was created as an exercise to understand how Kubernetes controllers work and interact with the cluster and resources.
+
+## Running
+
+```
+$ git clone https://github.com/trstringer/k8s-controller-core-resource
+$ cd k8s-controller-core-resource
+$ go run *.go
+```
+
+## Inspecting resources in the handler
+
+You are welcome to dump the resources themselves in handler but logging would be extremely verbose (and not interactive). I recommend you use a debugger...
+
+```
+$ dlv debug
+(dlv) b main.ObjectCreated
+(dlv) c
+```
+
+You can then trigger an event by creating a deployment of nginx...
+
+```
+$ kubectl run nginx --image=nginx
+```
+
+The breakpoint should be hit and you can analyze in the debugger the object...
+
+```
+(dlv) p obj
+```
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	log "github.com/Sirupsen/logrus"
+	api_v1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/workqueue"
+)
+
+// retrieve the Kubernetes cluster client from outside of the cluster
+func getKubernetesClient() kubernetes.Interface {
+	// construct the path to resolve to `~/.kube/config`
+	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
+
+	// create the config from the path
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
+
+	// generate the client based off of the config
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("getClusterConfig: %v", err)
+	}
+
+	log.Info("Successfully constructed k8s client")
+	return client
+}
+
+// main code path
+func main() {
+	// get the Kubernetes client for connectivity
+	client := getKubernetesClient()
+
+	// create the informer so that we can not only list resources
+	// but also watch them for all pods in the default namespace
+	informer := cache.NewSharedIndexInformer(
+		// the ListWatch contains two different functions that our
+		// informer requires: ListFunc to take care of listing and watching
+		// the resources we want to handle
+		&cache.ListWatch{
+			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
+				// list all of the pods (core resource) in the deafult namespace
+				return client.CoreV1().Pods(meta_v1.NamespaceDefault).List(options)
+			},
+			WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, error) {
+				// watch all of the pods (core resource) in the default namespace
+				return client.CoreV1().Pods(meta_v1.NamespaceDefault).Watch(options)
+			},
+		},
+		&api_v1.Pod{}, // the target type (Pod)
+		0,             // no resync (period of 0)
+		cache.Indexers{},
+	)
+
+	// create a new queue so that when the informer gets a resource that is either
+	// a result of listing or watching, we can add an idenfitying key to the queue
+	// so that it can be handled in the handler
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	// add event handlers to handle the three types of events for resources:
+	//  - adding new resources
+	//  - updating existing resources
+	//  - deleting resources
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			// convert the resource object into a key (in this case
+			// we are just doing it in the format of 'namespace/name')
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			log.Infof("Add pod: %s", key)
+			if err == nil {
+				// add the key to the queue for the handler to get
+				queue.Add(key)
+			}
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			key, err := cache.MetaNamespaceKeyFunc(newObj)
+			log.Infof("Update pod: %s", key)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+		DeleteFunc: func(obj interface{}) {
+			// DeletionHandlingMetaNamsespaceKeyFunc is a helper function that allows
+			// us to check the DeletedFinalStateUnknown existence in the event that
+			// a resource was deleted but it is still contained in the index
+			//
+			// this then in turn calls MetaNamespaceKeyFunc
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			log.Infof("Delete pod: %s", key)
+			if err == nil {
+				queue.Add(key)
+			}
+		},
+	})
+
+	// construct the Controller object which has all of the necessary components to
+	// handle logging, connections, informing (listing and watching), the queue,
+	// and the handler
+	controller := Controller{
+		logger:    log.NewEntry(log.New()),
+		clientset: client,
+		informer:  informer,
+		queue:     queue,
+		handler:   &TestHandler{},
+	}
+
+	// use a channel to synchronize the finalization for a graceful shutdown
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	// run the controller loop to process items
+	go controller.Run(stopCh)
+
+	// use a channel to handle OS signals to terminate and gracefully shut
+	// down processing
+	sigTerm := make(chan os.Signal, 1)
+	signal.Notify(sigTerm, syscall.SIGTERM)
+	signal.Notify(sigTerm, syscall.SIGINT)
+	<-sigTerm
+}
